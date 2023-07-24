1a6cafb88e894a0e0a7a2731ac46a274c367b650
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-24 16:51:24 +0800

    ui: welcome page ui

---
+++ task_1/resources/views/components/button.blade.php
@@ -0,0 +1,4 @@
+@props(['href'])
+<a href="{{ $href }}" class="rounded px-4 py-1 text-white bg-black">
+    {{ $slot }}
+</a>
---
+++ task_1/resources/views/components/layout.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Yashio Internship Task</title>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    </head>
+    <body class="w-full h-screen flex items-center justify-center">
+        {{ $slot }}
+    </body>
+</html>
