5744a2172f847d23003fe06e86b04798571d4bb2
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-31 11:25:16 +0300

    set up vuejs app with routing

--- task_2/resources/js/app.js
+++ task_2/resources/js/app.js
@@ -1,1 +1,25 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+// import components
+import App from './components/App.vue';
+import Post from './components/Post.vue';
+import PostForm from './components/PostForm.vue';
+import Posts from './components/Posts.vue';
+
+// vue-router
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: Posts },
+        { path: '/posts/:id', component: Post },
+        { path: '/posts/create', component: PostForm },
+        { path: '/posts/:id/edit', component: PostForm },
+    ],
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
