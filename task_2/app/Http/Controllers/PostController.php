6394dcf76747abfe168950f4acdf025704e6be97
Akuya Ekorot <akuyaekorot@gmail.com>
2023-07-31 16:42:13 +0300

    create new blog page

--- task_2/app/Http/Controllers/PostController.php
+++ task_2/app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        return Post::all();
+        return Post::orderBy('created_at', 'desc')->get();
     }
 
     /**
--- task_2/resources/js/components/PostForm.vue
+++ task_2/resources/js/components/PostForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="w-full flex flex-col items-center py-6">
+  <form @submit.prevent="submitForm" class="w-full flex flex-col items-center py-6">
     <div class="space-y-12 w-full max-w-3xl">
       <div class="border-b border-gray-900/10 pb-12">
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -7,7 +7,7 @@
             <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="title" id="title" class="pl-6 block flex-1 border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Blog title" />
+                <input v-model="post.title" required type="text" name="title" id="title" class="pl-6 block flex-1 border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="Blog title" />
               </div>
             </div>
           </div>
@@ -16,7 +16,7 @@
             <label for="slug" class="block text-sm font-medium leading-6 text-gray-900">Slug</label>
             <div class="mt-2">
               <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="slug" id="slug" class="pl-6 block flex-1 border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="blog-title" />
+                <input v-model="post.slug" required type="text" name="slug" id="slug" class="pl-6 block flex-1 border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="blog-title" />
               </div>
             </div>
           </div>
@@ -24,7 +24,7 @@
           <div class="col-span-full">
             <label for="excerpt" class="block text-sm font-medium leading-6 text-gray-900">Excerpt</label>
             <div class="mt-2">
-              <textarea id="excerpt" name="excerpt" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <textarea v-model="post.excerpt" required id="excerpt" name="excerpt" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
             <p class="mt-3 text-sm leading-6 text-gray-600">Write an excerpt that captures the essence of your post.</p>
           </div>
@@ -32,7 +32,7 @@
           <div class="col-span-full">
             <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
             <div class="mt-2">
-              <textarea id="body" name="body" rows="8" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+              <textarea v-model="post.body" required id="body" name="body" rows="8" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             </div>
             <p class="mt-3 text-sm leading-6 text-gray-600">Write your blog.</p>
           </div>
@@ -48,4 +48,46 @@
 </template>
 
 <script>
+    import axios from 'axios';
+
+    export default {
+        name: "PostForm",
+        data() {
+            return {
+                post: {
+                    title: '',
+                    slug: '',
+                    excerpt: '',
+                    body: '',
+                }
+            }
+        },
+        computed: {
+            isNewPost() {
+                return !this.$route.path.includes('edit');
+            }
+        },
+        async created() {
+            if (!this.isNewPost) {
+                const postResponse = await axios.get(`/api/post/${this.$route.params.id}`);
+                this.post = postResponse.data;
+            }
+        },
+        methods: {
+            async submitForm() {
+                try {
+                    if (this.isNewPost) {
+                        console.log(this.post);
+                        await axios.post('/api/posts', this.post);
+                    } else {
+                        await axios.put(`/api/posts/${this.$route.params.id}`, this.post);
+                    }
+
+                    this.$router.push('/');
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        }
+    }
 </script>
