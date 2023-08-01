9a9928ce6db6795e38ca90449c6f0376ed9e61ea
Akuya Ekorot <akuyaekorot@gmail.com>
2023-08-01 09:09:36 +0300

    filter by category

--- task_2/resources/js/components/Categories.vue
+++ task_2/resources/js/components/Categories.vue
@@ -2,13 +2,12 @@
     <div class="bg-white py-24 sm:py-32">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="mx-auto lg:mx-0 flex flex-row justify-between">
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">The blog</h2>
+                <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ category.name }}</h2>
             </div>
             <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                <article v-for="post in posts" :key="post.id" class="flex max-w-xl flex-col items-start hover:shadow-md p-4 rounded">
+                <article v-for="post in category.posts" :key="post.id" class="flex max-w-xl flex-col items-start hover:shadow-md p-4 rounded">
                     <div class="flex flex-col items-center gap-y-4 text-xs">
                         <time :datetime="post.created_at" class="text-gray-500">{{ new Date(post.created_at).toLocaleDateString("en-US") }}</time>
-                        <a :href="`categories/${post.category.slug}`" class="relative z-10 rounded-full bg-gray-100 px-2 py-1 font-medium text-gray-600 hover:bg-gray-300">{{ post.category.name }}</a>
                     </div>
                     <div class="group relative">
                         <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
@@ -31,14 +30,14 @@
         name: "Cateogories",
         data() {
             return {
-                posts: []
+                category: {}
             }
         },
         methods: {
             async fetchPosts() {
-                const postsResponse = await axios.get(`/api/categories/${this.$route.params.id}`);
-                console.log(postsResponse.data);
-                this.posts = postsResponse.data;
+                const categoryResponse = await axios.get(`/api/categories/${this.$route.params.id}`);
+                console.log(categoryResponse.data);
+                this.category = categoryResponse.data[0];
             },
         },
         mounted() {
