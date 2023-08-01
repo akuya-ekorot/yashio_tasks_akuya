e8ca766badc4309d0448e2efc6ab7c1d90cb2f4b
Akuya Ekorot <akuyaekorot@gmail.com>
2023-08-01 20:08:40 +0300

    input components

--- task_2/resources/js/components/CustomInputGroup.vue
+++ task_2/resources/js/components/CustomInputGroup.vue
@@ -1,17 +1,11 @@
 <template>
     <template v-if="type === 'input'">
-        <div v-if="type === 'input'" class="sm:col-span-4">
-            <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
-            <div class="mt-2">
-                <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                    <Input
-                        v-model="formStore.post[`${name}`]"
-                        :name="name"
-                        :placeholder="placeholder"
-                    />
-                </div>
-            </div>
-        </div>
+        <InputGroup
+            v-model="formStore.post[`${name}`]"
+            :name="name"
+            :formStore="formStore"
+            :placeholder="placeholder"
+        />
     </template>
     <template v-else-if="type === 'textarea'">
         <div class="col-span-full">
@@ -31,7 +25,8 @@
             <label for="category" class="block text-sm font-medium leading-6 text-gray-900">Category</label>
             <select class="mt-2" v-model="formStore.post.category">
                 <option
-                v-for="(category, index) in formStore.categories" :key="category.id"
+                    v-for="(category) in formStore.categories"
+                    :key="category.id"
                 >
                 {{category.name}}
                 </option>
@@ -43,5 +38,7 @@
 <script setup>
     import Input from './Input.vue';
     import TextArea from './TextArea.vue';
+    import InputGroup from './InputGroup.vue';
+
     defineProps(['type', 'formStore', 'name', 'title', 'placeholder']);
 </script>
---
+++ task_2/resources/js/components/InputGroup.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="sm:col-span-4">
+        <label :for="name" class="block text-sm font-medium leading-6 text-gray-900">{{ name.substring(0,1).toUpperCase() + name.substring(1) }}</label>
+		<div class="mt-2">
+			<div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+				<Input
+					v-model="formStore.post[`${name}`]"
+					:name="name"
+					:placeholder="placeholder"
+				/>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+    defineProps(['modelValue', 'formStore', 'name', 'placeholder']);
+    defineEmits(['update:modelValue']);
+</script>
