d41637a34f1ed20580620548ee1008f3d0b5207b
Akuya Ekorot <akuyaekorot@gmail.com>
2023-08-02 10:00:25 +0300

    api: migrate posts component to composition api

--- task_2/resources/js/components/Posts.vue
+++ task_2/resources/js/components/Posts.vue
@@ -14,7 +14,7 @@
                 </router-link>
             </div>
             <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                <article v-for="post in posts" :key="post.id" class="flex max-w-xl flex-col items-start hover:shadow-md p-4 rounded">
+                <article v-for="post in postsStore.posts" :key="post.id" class="flex max-w-xl flex-col items-start hover:shadow-md p-4 rounded">
                     <div class="flex flex-col items-center gap-y-4 text-xs">
                         <time :datetime="post.created_at" class="text-gray-500">{{ new Date(post.created_at).toLocaleDateString("en-US") }}</time>
                         <a :href="`/categories/${post.category.id}`" class="relative z-10 rounded-full bg-gray-100 px-2 py-1 font-medium text-gray-600 hover:bg-gray-300">{{ post.category.name }}</a>
@@ -33,24 +33,18 @@
     </div>
 </template>
 
-<script>
+<script setup>
     import axios from 'axios';
+    import { reactive } from 'vue';
 
-    export default {
-        name: "Posts",
-        data() {
-            return {
-                posts: []
-            }
-        },
-        methods: {
-            async fetchPosts() {
-                const postsResponse = await axios.get('/api/posts');
-                this.posts = postsResponse.data;
-            },
-        },
-        mounted() {
-            this.fetchPosts();
-        },
+    const postsStore = reactive({
+        posts: [],
+    });
+
+    const fetchPosts = async () => {
+        const postsResponse = await axios.get('/api/posts');
+        postsStore.posts = postsResponse.data;
     }
+
+    fetchPosts();
 </script>
