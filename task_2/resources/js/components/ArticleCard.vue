3ddaaf89519db83d0a4612c5b5e36d17cbaa9a8c
Akuya Ekorot <akuyaekorot@gmail.com>
2023-08-02 11:32:51 +0300

    ui: create article card component

---
+++ task_2/resources/js/components/ArticleCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <article v-for="post in postsStore.posts" :key="post.id" class="flex max-w-xl flex-col items-start hover:shadow-md p-4 rounded">
+        <div class="flex flex-col items-center gap-y-4 text-xs">
+            <time :datetime="post.created_at" class="text-gray-500">{{ new Date(post.created_at).toLocaleDateString("en-US") }}</time>
+            <a :href="`/categories/${post.category.id}`" class="relative z-10 rounded-full bg-gray-100 px-2 py-1 font-medium text-gray-600 hover:bg-gray-300">{{ post.category.name }}</a>
+        </div>
+        <div class="group relative">
+            <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                <router-link :to="`/posts/${post.id}`">
+                    {{ post.title }}
+                </router-link>
+            </h3>
+            <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{{ post.excerpt }}</p>
+        </div>
+    </article>
+</template>
+
+<script setup>
+    import { inject } from 'vue';
+
+    const postsStore = inject('postsStore');
+</script>
--- task_2/resources/js/components/Posts.vue
+++ task_2/resources/js/components/Posts.vue
@@ -14,20 +14,7 @@
                 </router-link>
             </div>
             <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                <article v-for="post in postsStore.posts" :key="post.id" class="flex max-w-xl flex-col items-start hover:shadow-md p-4 rounded">
-                    <div class="flex flex-col items-center gap-y-4 text-xs">
-                        <time :datetime="post.created_at" class="text-gray-500">{{ new Date(post.created_at).toLocaleDateString("en-US") }}</time>
-                        <a :href="`/categories/${post.category.id}`" class="relative z-10 rounded-full bg-gray-100 px-2 py-1 font-medium text-gray-600 hover:bg-gray-300">{{ post.category.name }}</a>
-                    </div>
-                    <div class="group relative">
-                        <h3 class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                            <router-link :to="`/posts/${post.id}`">
-                                {{ post.title }}
-                            </router-link>
-                        </h3>
-                        <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{{ post.excerpt }}</p>
-                    </div>
-                </article>
+                <ArticleCard />
             </div>
         </div>
     </div>
@@ -35,11 +22,16 @@
 
 <script setup>
     import axios from 'axios';
-    import { reactive } from 'vue';
+    import { reactive, provide } from 'vue';
+
+    // components
+    import ArticleCard from './ArticleCard.vue';
 
     const postsStore = reactive({
         posts: [],
     });
+
+    provide('postsStore', postsStore);
 
     const fetchPosts = async () => {
         const postsResponse = await axios.get('/api/posts');
